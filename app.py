4000f23f4d72edfe335473f76a7a3cada36e7848
Darshil Parikh <darshil.parikh@gmail.com>
2019-03-07 03:01:06 -0600

    removed credentials for making repo public

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
 import pyodbc as db
-import redis
+# import redis
 import pygal
 
 app = Flask(__name__)
-conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={insert db password here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 # redis_connect_dict = {}
 # redis_connect_dict['host'] = 'cloud3redis.redis.cache.windows.net'
 # redis_connect_dict['port'] = 6380
 # redis_connect_dict['db'] = 0
-# redis_connect_dict['password'] = 'iMB7hsBunvCnYzZPekiBk+ZAX3TzDKzDplY+Pc8Y2+s='
+# redis_connect_dict['password'] = 'insert redis password here'
 #
 # r = redis.StrictRedis(redis_connect_dict['host'],
 #                       redis_connect_dict['port'],
